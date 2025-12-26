ca0d41846d4c3192445a5c452f7031d4bcd8f372
Kusya Lakomkin <zubareff.4lex@yandex.ru>
2025-12-27 00:35:48 +0300

    feat: implement distributions

--- lib/distributions/CauchyDistribution.cpp
+++ lib/distributions/CauchyDistribution.cpp
@@ -1,1 +1,35 @@
+#include <cmath>
+#include <numbers>
+#include <stdexcept>
 #include "CauchyDistribution.hpp"
+
+namespace ptm {
+CauchyDistribution::CauchyDistribution(double x0, double gamma) : x0_(x0), gamma_(gamma) {
+    if (gamma <= 0) {
+        throw std::invalid_argument("gamma must be > 0");
+    }
+}
+
+double CauchyDistribution::Pdf(double x) const {
+    double coeff = (x - x0_) * (x - x0_) + gamma_ * gamma_;
+    return 1 / (std::numbers::pi * coeff);
+}
+
+double CauchyDistribution::Cdf(double x) const {
+    double coeff = atan((x - x0_) / gamma_);
+    return 1 / std::numbers::pi * coeff + 0.5;
+}
+
+double CauchyDistribution::Sample(std::mt19937& rng) const {
+    double u = (static_cast<double>(rng()) + 0.5) / (static_cast<double>(rng.max()) + 1);
+    return x0_ + gamma_ * std::tan(std::numbers::pi * (u - 0.5));
+}
+
+double CauchyDistribution::TheoreticalMean() const {
+    return NAN;
+}
+
+double CauchyDistribution::TheoreticalVariance() const {
+    return NAN;
+}
+}
--- lib/distributions/UniformDistribution.cpp
+++ lib/distributions/UniformDistribution.cpp
@@ -1,1 +1,40 @@
+#include <cmath>
+#include <stdexcept>
 #include "UniformDistribution.hpp"
+
+namespace ptm {
+UniformDistribution::UniformDistribution(double a, double b) : a_(a), b_(b) {}
+
+double UniformDistribution::Pdf(double x) const {
+     if ( a_ <= x <= b_) {
+        return 1 / (b_ - a_);
+     }
+
+     return 0;
+}
+
+double UniformDistribution::Cdf(double x) const {
+    if ( x < a_) {
+        return 0;
+    }
+
+    if ( x >= b_) {
+        return 1;
+    }
+
+    return (x - a_) / (b_ - a_);
+}
+
+double UniformDistribution::Sample(std::mt19937& rng) const {
+    double u = (static_cast<double>(rng()) + 0.5) / (static_cast<double>(rng.max()) + 1);
+    return a_ + (b_ - a_) * u;
+}
+
+double UniformDistribution::TheoreticalMean() const {
+    return (a_ + b_) / 2;
+}
+
+double UniformDistribution::TheoreticalVariance() const {
+    return (b_ - a_) * (b_ - a_) / 12;
+}
+} // namespace ptm
